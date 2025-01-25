4e7b08a6bf98767b10f742b317aa0a8014053868
Riley-Neuville <Riley.Neuville@gmail.com>
2025-01-25 15:35:58 -0600

    added modals when party names are clicked on

--- my-react-app/src/components/Member.js
+++ my-react-app/src/components/Member.js
@@ -2,13 +2,27 @@ import React, { useState } from "react";
 import "../Member.css";
 
 function Member(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <>
-      <div className="member">
+      <div className="member" onClick={handleModalToggle}>
         <h1>{props.name}</h1>
         <p>{props.description}</p>
       </div>
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={handleModalToggle}>
+          <div className="modal-content">
+            <img src={props.imageSrc} alt={`${props.name}`} />
+          </div>
+        </div>
+      )}
     </>
   );
 }
+
 export default Member;
--- my-react-app/src/pages/partyPage.js
+++ my-react-app/src/pages/partyPage.js
@@ -10,22 +10,78 @@ function PartyPage() {
       <NavigationMenu />
       <div className="partyGrid">
         <div className="partyM">
-          <Member name="Matt Gall" description="Best Man" />
-          <Member name="Kyle Neuville" description="Brother of the Groom" />
-          <Member name="Connor Neuville" description="Brother of the Groom" />
-          <Member name="Ryan Neuville" description="Brother of the Groom" />
-          <Member name="Joe Craven" description="Groomsmen" />
-          <Member name="Greg Farnsworth" description="Groomsmen" />
-          <Member name="Bryan Hoyt" description="Groomsmen" />
+          <Member
+            name="Matt Gall"
+            description="Best Man"
+            imageSrc={`${process.env.PUBLIC_URL}/images/NOR2242-HDR.jpg`}
+          />
+          <Member
+            name="Kyle Neuville"
+            description="Brother of the Groom"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Kyle.jpeg`}
+          />
+          <Member
+            name="Connor Neuville"
+            description="Brother of the Groom"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Connor.jpeg`}
+          />
+          <Member
+            name="Ryan Neuville"
+            description="Brother of the Groom"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Ryan.jpeg`}
+          />
+          <Member
+            name="Joe Craven"
+            description="Groomsmen"
+            imageSrc={`${process.env.PUBLIC_URL}/images/NOR2242-HDR.jpg`}
+          />
+          <Member
+            name="Greg Farnsworth"
+            description="Groomsmen"
+            imageSrc={`${process.env.PUBLIC_URL}/images/NOR2242-HDR.jpg`}
+          />
+          <Member
+            name="Bryan Hoyt"
+            description="Groomsmen"
+            imageSrc={`${process.env.PUBLIC_URL}/images/NOR2242-HDR.jpg`}
+          />
         </div>
         <div className="partyW">
-          <Member name="Alexandra Rares" description="Maid of honor" />
-          <Member name="Brittney Hearn" description="Sister of the Bride" />
-          <Member name="Jennifer Dengel" description="Sister of the Bride" />
-          <Member name="Kelly Burke" description="Sister of the Bride" />
-          <Member name="Dani Gall" description="Bridesmaid" />
-          <Member name="Kayla Gregory" description="Bridesmaid" />
-          <Member name="Nicole Bernardi" description="Bridesmaid" />
+          <Member
+            name="Alexandra Rares"
+            description="Maid of honor"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Alexandra.jpeg`}
+          />
+          <Member
+            name="Brittney Hearn"
+            description="Sister of the Bride"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Britt.jpeg`}
+          />
+          <Member
+            name="Jennifer Dengel"
+            description="Sister of the Bride"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Jen.jpeg`}
+          />
+          <Member
+            name="Kelly Burke"
+            description="Sister of the Bride"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Kelly.jpeg`}
+          />
+          <Member
+            name="Dani Gall"
+            description="Bridesmaid"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Dani.jpeg`}
+          />
+          <Member
+            name="Kayla Gregory"
+            description="Bridesmaid"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Kayla.jpeg`}
+          />
+          <Member
+            name="Nicole Bernardi"
+            description="Bridesmaid"
+            imageSrc={`${process.env.PUBLIC_URL}/images/Nicole.jpeg`}
+          />
         </div>
       </div>
     </div>
