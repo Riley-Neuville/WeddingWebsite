4232036709cfcaef00cbdd236b686955a74af044
Riley-Neuville <Riley.Neuville@gmail.com>
2025-01-28 11:54:02 -0600

    fixed scrolltotop by adding 50ms timeout

--- my-react-app/src/components/ScrollToTop.js
+++ my-react-app/src/components/ScrollToTop.js
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+  useLayoutEffect(() => {
+    // Adding a small delay using setTimeout to ensure DOM is ready
+    setTimeout(() => {
+      console.log("Scrolling to top:", pathname);
+      window.scrollTo(0, 0); // Scroll to top of the page
+    }, 50); // 100ms delay to allow for page rendering
+  }, [pathname]); // Only re-run when the pathname changes
 
   return null;
 };
